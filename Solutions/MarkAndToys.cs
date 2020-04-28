6f0628c8d24d91bd5c32ac480609db1e3cd86059
Mark Nyder <mneider.virginia@gmail.com>
2020-04-28 18:14:02 -0500

    solve mark and toys

--- Solutions/MarkAndToys.cs
+++ Solutions/MarkAndToys.cs
@@ -12,9 +12,82 @@ namespace Solutions
     {
         public static int MaximumToys(int[] prices, int k)
         {
-             
+            int[] sortedPrices = QuickSort(prices);
 
-            return 0;
+            int spent = 0;
+            int toyCount = -1;
+
+            while (spent < k && toyCount < sortedPrices.Length)
+            {
+                toyCount++;
+                spent += sortedPrices[toyCount];
+            }
+
+            return toyCount;
+        }
+
+        /// <summary>
+        /// sort input using a quicksort
+        /// https://en.wikipedia.org/wiki/Quicksort
+        /// </summary>
+        /// <typeparam name="T">type of input array</typeparam>
+        /// <param name="unsorted">unsorted array</param>
+        /// <returns>sorted array</returns>
+        public static int[] QuickSort(int[] unsorted)
+        {
+            Random random = new Random();
+
+            int[] sorting = new int[unsorted.Length];
+            Array.Copy(unsorted, sorting, unsorted.Length);
+
+            QuickSort(sorting, 0, sorting.Length - 1, random);
+
+            return sorting;
+        }
+
+        private static void QuickSort(int[] sorting, int low, int high, Random random)
+        {
+            if (low < high)
+            {
+                int partition = Partition(sorting, low, high, random);
+                QuickSort(sorting, low, partition, random);
+                QuickSort(sorting, partition + 1, high, random);
+            }
+        }
+
+        private static int Partition(int[] sorting, int low, int high, Random random)
+        {
+            int pivotIndex = random.Next(low, high + 1);
+            int pivot = sorting[pivotIndex];
+
+            int i = low - 1;
+            int j = high + 1;
+
+            while (true)
+            {
+                do
+                {
+                    i++;
+                }
+                while (sorting[i] < pivot);
+                do
+                {
+                    j--;
+                } while (sorting[j] > pivot);
+
+                if (i < j)
+                {
+                    int temp = sorting[i];
+                    sorting[i] = sorting[j];
+                    sorting[j] = temp;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return j;
         }
     }
 }
