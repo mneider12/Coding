f5e6fd017a36ef52b9c0a165c1f3a7440012324a
Mark Nyder <mneider.virginia@gmail.com>
2020-04-28 19:06:38 -0500

    solve drawing book

---
+++ Solutions/DrawingBook.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solutions
+{
+    /// <summary>
+    /// Solution for drawing book question on hackerrank
+    /// https://www.hackerrank.com/challenges/drawing-book/problem
+    /// </summary>
+    public static class DrawingBook
+    {
+        public static int PageCount(int n, int p)
+        {
+            return 2 * (p / 2) > n / 2 ? (n / 2) - (p / 2) : p / 2;
+        }
+    }
+}
---
+++ UnitTests/DrawingBookTests.cs
@@ -0,0 +1,25 @@
+﻿using Solutions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace SolutionTests
+{
+    public class DrawingBookTests
+    {
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void DrawingBookTest(int pages, int page, int flips)
+        {
+            Assert.Equal(flips, DrawingBook.PageCount(pages, page));
+        }
+
+        public static IEnumerable<object[]> Data = new List<object[]>()
+        {
+            new object[] { 6, 2, 1 },
+            new object[] { 5, 4, 0 },
+        };
+
+    }
+}
